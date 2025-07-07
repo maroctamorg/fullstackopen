6548dd4ad8d63fc9d713c6ce4ee6ece772bf07d8
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2025-07-07 22:13:35 +0300

    exercise 7.9: prettier setup + eslint config

--- part7/bloglist/backend/eslint.config.mjs
+++ part7/bloglist/backend/eslint.config.mjs
@@ -1,59 +1,42 @@
-import globals from "globals"
-import stylisticJs from "@stylistic/eslint-plugin-js"
+import globals from 'globals'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.js"],
-    languageOptions:
+    js.configs.recommended,
     {
-      sourceType: "commonjs",
-      globals: {
-        ...globals.node,
-      },
-      ecmaVersion: "latest",
+        files: ['**/*.js'],
+        languageOptions: {
+            sourceType: 'commonjs',
+            globals: {
+                ...globals.node,
+            },
+            ecmaVersion: 'latest',
+        },
+        plugins: {
+            '@stylistic/js': stylisticJs,
+        },
+        rules: {
+            '@stylistic/js/indent': ['error', 4],
+            '@stylistic/js/linebreak-style': ['error', 'unix'],
+            '@stylistic/js/quotes': ['error', 'single'],
+            '@stylistic/js/semi': ['error', 'never'],
+            eqeqeq: 'error',
+            'no-trailing-spaces': 'error',
+            'object-curly-spacing': ['error', 'always'],
+            'arrow-spacing': [
+                'error',
+                {
+                    before: true,
+                    after: true,
+                },
+            ],
+            'no-console': 'off',
+        },
     },
-    plugins: {
-      '@stylistic/js': stylisticJs
+    {
+        ignores: ['build/**', 'dist/**'],
     },
-    rules: {
-      '@stylistic/js/indent': [
-        'error',
-        4
-      ],
-      '@stylistic/js/linebreak-style': [
-        'error',
-        'unix'
-      ],
-      '@stylistic/js/quotes': [
-        'error',
-        'single'
-      ],
-      '@stylistic/js/semi': [
-        'error',
-        'never'
-      ],
-      'eqeqeq': 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': [
-        'error',
-        'always'
-      ],
-      'arrow-spacing': [
-        'error',
-        {
-          before: true,
-          after: true
-        }
-      ],
-      'no-console': 'off',
-    },
-  },
-  {
-    ignores: [
-      'build/**',
-      'dist/**',
-    ]
-  }
-];
+    eslintConfigPrettier,
+]
