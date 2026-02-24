184d427a0452e8a39c8aa6dd2ac51a38335e65fb
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2026-02-24 11:02:17 +0200

    exercise 13.2

--- part13/commands.sql
+++ part13/commands.sql
@@ -5,3 +5,26 @@ CREATE TABLE blogs (
     title VARCHAR(255) NOT NULL,
     likes INTEGER NOT NULL DEFAULT 0
 );
+
+INSERT INTO
+    blogs (author, url, title, likes)
+VALUES (
+        'John Doe',
+        'https://example.com/blog1',
+        'First Blog Post',
+        10
+    ),
+    (
+        'Jane Smith',
+        'https://example.com/blog2',
+        'Second Blog Post',
+        20
+    ),
+    (
+        'Alice Johnson',
+        'https://example.com/blog3',
+        'Third Blog Post',
+        5
+    );
+
+SELECT * FROM blogs;
